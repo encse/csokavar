5ebcd1c1452253b61e1c407f4f05fdfbe3aba14a
david.nemeth.csoka <david.nemeth.csoka@prezi.com>
2014-07-05 16:48:13 +0200

    q

--- content.php
+++ content.php
@@ -42,7 +42,6 @@
 
 		<div class="entry-meta">
 			<?php twentythirteen_entry_meta(); ?>
-			<?php edit_post_link( __( 'Edit', 'twentythirteen' ), '<span class="edit-link">', '</span>' ); ?>
 		</div><!-- .entry-meta -->
 		
 		<?php echo sharing_display(); ?>
