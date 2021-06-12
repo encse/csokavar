b8f1edd4512eb816472bb8af35805a19ce9b4796
David Nemeth Csoka <encse@csokavar.hu>
2021-06-12 07:45:24 +0200

    typo

--- src/baseStyle.ts
+++ src/baseStyle.ts
@@ -1,7 +1,7 @@
 import {fontFace as fontAwesome} from './components/fontAwesame';
 
 export const linkColor = '#1d59d1';
-export const xxlFontSize = 'var(--xxl-text-size:)';
+export const xxlFontSize = 'var(--xxl-text-size)';
 export const textColor = '#c8c8c8';
 export const backgroundColor = '#1e1e1e';
 
